cc548c6e5885c32b471c643ae97785072f48c763
Łukasz Włodarczyk <ukasz Wodarczyk>
2016-03-08 10:18:34 +0100

    maven

---
+++ unittestdemo/src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+	
+	public int add(int l1,int l2){
+		return l1+l2;
+	}
+	public int sub(int l1,int l2){
+		return l1-l2;
+	}
+	public int div(int l1,int l2){
+		return l1/l2;
+	}
+	public int mul(int l1,int l2){
+		return l1*l2;
+	}
+	public boolean greater(int l1,int l2){
+		return(l1>l2) ? true : false;
+	}
+	
+}
+
---
+++ unittestdemo/src/test/java/calculator/test/CalculatorTest.java
@@ -0,0 +1,52 @@
+package calculator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import calculator.Calculator;
+
+public class CalculatorTest {
+	
+	static Calculator cal;
+	
+	@BeforeClass
+	public static void globalSetUp(){
+		cal = new Calculator();
+	}
+	
+	@Test
+	public void testAdd(){
+		assertEquals(10,cal.add(5, 5));
+	}
+	
+	@Test
+	public void testSub(){
+		assertEquals(10,cal.sub(15, 5));
+	}
+	
+	@Test
+	public void testMul(){
+		assertEquals(10,cal.mul(2, 5));
+		assertEquals(-10,cal.mul(-2, 5));
+		assertEquals(10,cal.mul(-2, -5));
+	}
+	
+	@Test
+	public void testDiv(){
+		assertEquals(3,cal.div(15, 5));
+		assertEquals(3,cal.div(-15, -5));
+	}
+	
+	@Test
+	public void testGreater(){
+		assertTrue(cal.greater(5, 4));
+	}
+	
+	@Test(expected = ArithmeticException.class) 
+	public void testAssert(){
+		cal.div(5, 0);
+	}
+	
+}
