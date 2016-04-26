07c410cac8f8b18b9a715509bf8c29d99cc99a1d
Łukasz Włodarczyk <ukasz Wodarczyk>
2016-04-26 09:05:03 +0200

    selenium

--- lab05/src/test/java/com/example/webguidemo/Gol24Steps.java
+++ lab05/src/test/java/com/example/webguidemo/Gol24Steps.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.openqa.selenium.By;
 
 public class Gol24Steps {
 	
@@ -39,8 +40,11 @@ public class Gol24Steps {
     @Then("Login error is shown")
     public void errorIsShown(){
 
-       //assertEquals(true,pages.login().findElement(By.className("komunikat blad")).isDisplayed());
-
+       assertEquals("Podany email lub has³o nie s¹ poprawne. Upewnij siê, ¿e aktywowa³eœ konto poprzez link, który wys³aliœmy na adres e-mail, i spróbuj ponownie."
+    		   ,pages.login().findElement(By.cssSelector(".komunikat > p:nth-child(1)")).getText());
+    	
+    	
+    	
 
 
     }
